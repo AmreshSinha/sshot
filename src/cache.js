cf58b18c9453c164a5a1ecbe7d3fff2d3f20864e
AmreshSinha <theamresh.sinha@gmail.com>
2022-02-06 20:36:07 +0530

    Add expiring capibility

--- src/cache.js
+++ src/cache.js
@@ -5,6 +5,7 @@ module.exports = {
   checkCache: async (url) => {
     let returnedJson = await client.client.get(url);
     if (returnedJson) {
+      client.client.expire(url, 60*60*24);
       return JSON.parse(returnedJson);
     } else {
       return false;
@@ -13,6 +14,7 @@ module.exports = {
 
   setcache: async (url, json) => {
     await client.client.set(url, JSON.stringify(json));
+    await client.client.expire(url, 60*60*24);
     json = await client.client.get(url);
     await json;
     return JSON.parse(json);
