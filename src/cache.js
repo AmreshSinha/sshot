b6bc2e526bb37298ceec3246d6327b7b4e6e9666
AmreshSinha <theamresh.sinha@gmail.com>
2022-02-06 21:43:25 +0530

    Improve Caching, Naming, and etc stuff

--- src/cache.js
+++ src/cache.js
@@ -2,20 +2,20 @@ const client = require("../app");
 require("dotenv").config();
 
 module.exports = {
-  checkCache: async (url) => {
-    let returnedJson = await client.client.get(url);
+  checkCache: async (query) => {
+    let returnedJson = await client.client.get(query);
     if (returnedJson) {
-      client.client.expire(url, 60*60*24);
+      client.client.expire(query, 60*60*24);
       return JSON.parse(returnedJson);
     } else {
       return false;
     }
   },
 
-  setcache: async (url, json) => {
-    await client.client.set(url, JSON.stringify(json));
-    await client.client.expire(url, 60*60*24);
-    json = await client.client.get(url);
+  setcache: async (query, json) => {
+    await client.client.set(query, JSON.stringify(json));
+    await client.client.expire(query, 60*60*24);
+    json = await client.client.get(query);
     await json;
     return JSON.parse(json);
   },
